d40a3ebde43337330902e45d45c3fa90cfe934ee
Juicy1208 <100836032+Juicy1208@users.noreply.github.com>
2022-05-04 23:48:04 +0800

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,11 +1,11 @@
 #找出留言長度小於100
 data = []
 with open('reviews.txt','r') as reviews:
-	for review in reviews:  
-		data.append(review)
+	for review in reviews:  #for字串in清單
+		data.append(review) #把字串丟入data清單
 small100 =[]
 for d in data:
-	if len(d)<100:
-		small100.append(d)
+	if len(d)<100: #篩選字串長度小於100的字串
+		small100.append(d) #長度小於100的存入small100清單
 print(len(small100))
 
